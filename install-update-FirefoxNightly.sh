fe0480aee0787786ee7066ee8da922ba84a2e711
Tvax <19556689+Tvax@users.noreply.github.com>
2017-08-14 13:53:45 +0200

    extact to tmp + timer

--- install-update-FirefoxNightly.sh
+++ install-update-FirefoxNightly.sh
@@ -2,7 +2,8 @@
 clear;
 echo "Close Firefox !";
 wget -O /tmp/firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux&lang=en-US";
-tar -xjvf firefox*;
+tar -xjvf /tmp/firefox* -C /tmp/;
 mv /tmp/firefox/ /tmp/firefox-nightly/;
 killall -9 firefox;
+sleep 10;
 sudo cp -r /tmp/firefox-nightly/ /opt/;
